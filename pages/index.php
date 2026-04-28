033cec5dd285c5b05db5c2b0830b3a27b573a8d3
Maria <salyginamaria156@gmail.com>
2026-04-28 17:11:01 +0300

    Добавлены стили редакторского контента

--- pages/index.php
+++ pages/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
-    <link rel="stylesheet" href="../css/style.css?v=3.0.1">
+    <link rel="stylesheet" href="../css/style.css?v=3.0.2">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
